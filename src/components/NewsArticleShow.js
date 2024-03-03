91d278cb105439662601bd1bcd6ad8d0f2718c2b
Peter Ogada <peterogada@Peters-MacBook-Air-2.local>
2024-03-03 17:14:11 -0500

    Added some more custom CSS to NewsArticleShow.css file I also added more logic to the NewsArticleShow.js component specifically specifically I added an onClick event handler

--- src/components/NewsArticleShow.js
+++ src/components/NewsArticleShow.js
@@ -1,21 +1,28 @@
 import '../css/NewsArticleShow.css'
 
 function NewsArticleShow({article}) {
+  
+  const handleClick = () => {
+    window.open(article.url, '_blank');
+  }
+
   return(
-    <div className='ml-5 mb-5'>
-      <div className='ml-5 title is-5 has-text-link'>
+    <div className='main-div ml-5'>
+
+      <div onClick = {handleClick} className='article-title ml-5 title is-5 has-text-link'>
         {article.title}
       </div>
 
-      <p className='ml-5'>The Hacker News {'>'} Cybersecurity News</p>
+      <p className='hacker-news ml-5'>The Hacker News {'>'} Cybersecurity News</p>
 
       <div className='container'>
         <div className='columns'>
           <div className='first-column column is-2'>
-            <div className='div-3'>
+            <div onClick={handleClick} className='div-3'>
               <img src={article.urlToImage}/>
             </div>
           </div>
+
           <div className='column is-10'>
             <div>
               {article.description}
